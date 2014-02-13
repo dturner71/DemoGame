d4ce0084d8c3f9a04de83441e33cec48a0f3c235
David Turner <drturner71@gmail.com>
2014-02-13 10:33:54 -0800

    adding repo

---
+++ src/screen/mainScreen.java
@@ -0,0 +1,11 @@
+package screen;
+
+import objectSelector.ObjectSelector;
+import objectSelector.ObjectTypes.SelectorType;
+import objectSelector.ObjectTypes.ObjectTags;
+
+public final class mainScreen {
+//public static ObjectSelector def = new ObjectSelector(ObjectTags.button, SelectorType.name, "", "", "placeHolder");
+public static ObjectSelector buttonProgressBar = new ObjectSelector(ObjectTags.button, SelectorType.name, "waitingButtonTestCD", "waitingButtonTestCD", "Show Progress Bar for a while");
+
+}
